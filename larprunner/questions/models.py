e962c027219bccfadf67bf24465828bb1db6801f
afriho <afriho@3c3773bc-7d4c-0410-acd0-0358cc0c1ac3>
2008-09-20 00:32:57 +0000

    added a mothod which creates a form element from a Question
    git-svn-id: http://larp-runner.googlecode.com/svn/trunk@20 3c3773bc-7d4c-0410-acd0-0358cc0c1ac3

--- larprunner/questions/models.py
+++ larprunner/questions/models.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
 
+from django import newforms as forms
 from django.db import models
 
 class QuestionManager(models.Manager):
@@ -23,12 +24,12 @@ class QuestionManager(models.Manager):
       question.comment = comment
 
     question.save()
-    
+
     ChoicesForQuestion.objects.filter(question=question).delete()
     for choice in choices:
       new_choice = ChoicesForQuestion(choice=choice, question = question)
       new_choice.save()
-    
+
     return question
 
 
@@ -38,7 +39,6 @@ QTYPES = (
            ('CHECKBOX',   'Checkbox'),
            ('RADIO',      'Radio button'),
            )
-          
 
 class Question(models.Model):
   uniq_name = models.CharField("Unikátní název", maxlength=30)
@@ -48,7 +48,28 @@ class Question(models.Model):
   comment   = models.TextField("Komentář")
 
   objects = QuestionManager()
-  
+
+  def asField(self):
+
+    choices = ChoicesForQuestion.objects.filter(question=self)
+    choices_prepared=[]
+    for choice in choices:
+      choices_prepared.append((choice.id, choice.choice))
+
+    if self.type == "TEXTFIELD":
+      field = forms.CharField(maxlen=self.maxlen,label=self.comment)
+    elif self.type == "TEXTAREA":
+      field = forms.CharField(widget=forms.Textarea, label=self.comment, )
+    elif self.type == "CHECKBOX":
+      field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,choices=choices_prepared,label=self.comment)
+    elif self.type == "RADIO":
+      field = forms.ChoiceField(choices=choices_prepared,label=self.comment)
+    return field
+
 class ChoicesForQuestion(models.Model):
   choice    = models.CharField(maxlength=50)
   question  = models.ForeignKey(Question)
+
+class Answer(models.Model):
+  question = models.ForeignKey(Question)
+  answer = models.TextField()
