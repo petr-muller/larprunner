9e7dbfa605bda8f78c3349f28cae24fd909d14c4
afriho <afriho@3c3773bc-7d4c-0410-acd0-0358cc0c1ac3>
2008-10-05 21:29:03 +0000

    Undoing commit r42, which was committed badly
    git-svn-id: http://larp-runner.googlecode.com/svn/trunk@43 3c3773bc-7d4c-0410-acd0-0358cc0c1ac3

--- Makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: clean server test
-
-server:
-	python manage.py runserver 8001
-
-clean: 
-	rm -f *.pyc *.pyo
-	rm -f admin/*.pyc admin/*.pyo
--- events/templatetags/tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def in_list(value,arg):
-  print "%s in %s" % (value, arg)
-  return value in arg
